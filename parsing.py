7ef0a007af7736204213a539299a4add2c05e914
Chelsea Sidrane <csidrane@stanford.edu>
2018-12-27 13:28:02 -0800

    working on parsing

--- parsing.py
+++ parsing.py
@@ -1,4 +1,7 @@
 # parsing code
+# right now, assume that we DONT take batches. we only take one "datapoint" at a time
+# assume multiplication of the form: W*x + b
+import numpy as np
 
 # Do I want to create a new TF network? Or just go directly to .nnet?
 # I think I want to condense to a new tf network, because then I can run some test inputs through both new and old and make sure they're the same...
@@ -21,35 +24,86 @@ def are_inputs_vars(op):
 # outputs: list of weights and biases for a flattened representation of the network 
 def parse_network(op, temp_W, temp_b, final_W, final_b, activation_type):
 	if not (op.type==activation_type):
-		input_varflags = are_inputs_vars(op)
-		# if op has more than 1 variable input:
-		if sum(input_varflags) > 1: 
-			# get input ops
-			iops = [oi.op for oi in op.inputs]
-			iop_varflags = [sum(are_inputs_vars(o)) for o in iops]
-			# if input ops just have 1 variable input each
-			if len(iops) == sum(iop_varflags):
-				# restack those two input ops into 1
-				restacked = matrix_stacker(iops)
-				# return: continue to recurse on inputs to newly stacked op
-				return parse_network()
-			# else if input ops have more than 1 variable input each...
-				# recurse on each input op with a "blank" tensor/matrix list
-				# restack input ops into a single op
-				# continue to recurse on inputs to newly stacked op
-		else # if op just has 1 variable input:
-			# include tensor in list
-			# recurse on input
+		# if there is more than 1 variable input
+		if sum(are_inputs_vars(op)) > 1:
+
+		else: # if there is only 1 variable input
+
 	else # if op is of type activation
 		# multiply temporary tensor list togeter to produce final tensor. add the final tensor list
 		# record activation type? into activation list?
 		# potentially record into .nnet file?
 		# recurse on input
 
+# find dim of vector that is not 1
+# assume 2D only
+def get_long_len(tfvec):
+	s = tfvec._shape_as_list()
+	if s[0] == 1:
+		return s[1]
+	else:
+		return s[0]
+
+def get_add_mat(op, sess):
+	n = get_long_len(op.inputs[0])
+	nprime = get_long_len(op.inputs[1])
+	assert n==nprime
+	if is_variable(op.inputs[0]) and is_variable(op.inputs[1]):
+		W11 = np.identity(n)
+		W12 = np.identity(n)
+		W = np.hstack([W11, W12])
+		b = np.zeros([n,1])
+	elif is_variable(op.inputs[0]):
+		W = np.identity(n)
+		with sess.as_default():
+			b = op.inputs[1].eval()
+	elif is_variable(op.inputs[1]):
+		W = np.identity(n)
+		with sess.as_default():
+			b = op.inputs[0].eval()
+	else: # neither is a variable. don't expect to use this case...
+		print("Why are we here?")
+		W11 = np.identity(n)
+		W12 = np.identity(n)
+		W = np.hstack([W11, W12])
+		b = np.zeros([n,1])
+	return (W,b)
+
+# assume R = W*x + b
+def get_matmul_mat(op,sess):
+	if is_variable(op.inputs[0]):
+		with sess.as_default():
+			W = op.inputs[1].eval()
+		n = W.shape[0]
+		b = np.zeros([n,1])
+	elif is_variable(op.inputs[1]):
+		with sess.as_default():
+			W = op.inputs[0].eval()
+		n = W.shape[0]
+		b = np.zeros([n,1])
+	elif is_variable(op.inputs[0]) and is_variable(op.inputs[1]):
+		raise ValueError('Both inputs to matmul are variables')
+	else: # both are constants
+		rause ValueError('Both inpust to matmul are constants. Do you want to implement this?')
+	return (W,b)
+
+# inputs: op
+# output: matmul and bias add representing that op
+def op_to_mat(op, sess):
+	if op.type == 'Add':
+		W,b = get_add_mat(op, sess)
+	elif op.type == 'MatMul'
+		W,b = get_matmul_mat(op, sess)
+	else:
+		print('op type:', op.type)
+		raise ValueError('op type not supported')
+	return (tf.constant(W), tf.constant(b))
+
+
 # return true if is derived of a variable
 # return false if only derived of constants
 # TODO: If only derived of constants but not DIRECTLY a constant...this can be squashed into a single constant...
-# I think tthere may be a tensorflow function to do this, because I tthink it happens when you "freeze" tthe network
+# I think tthere may be a tensorflow function to do this, because I tthink it happens when you "freeze" the network
 def is_variable(tensor):
 	flag = tensor.op.type == "VariableV2"
 	if flag:
@@ -57,10 +111,6 @@ def is_variable(tensor):
 	for i in tensor.op.inputs:
 		flag = flag or is_variable(i)
 	return flag
-
-# inputs: matrices
-# output: one signle stacked matrix
-def matrix_stacker(?):
 
 
 
--- simple_parsing.py
+++ simple_parsing.py
@@ -1,19 +1,18 @@
 import tensorflow as tf
-
 x = tf.Variable([[0.0], [0.0]])
 y = tf.Variable([[1.0], [1.0]])
-
 W1 = tf.constant([[2.0, 0.0],[0.0, 2.0]])
 b1 = tf.constant([[-1.0], [-1.0]])
-
 W2 = -W1
 b2 = -b1
 with tf.name_scope("matmul_1"):
 	z1 = W1@x + b1
 
+
 with tf.name_scope("matmul_2"):
 	z2 = W2@y + b2
 
+	
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
