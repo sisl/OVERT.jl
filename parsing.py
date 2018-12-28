43af3ce6271cf59823e2928d76290092dcc5e5aa
Chelsea Sidrane <csidrane@stanford.edu>
2018-12-27 23:29:44 -0800

    made progress on parsing code

--- parsing.py
+++ parsing.py
@@ -2,6 +2,7 @@
 # right now, assume that we DONT take batches. we only take one "datapoint" at a time
 # assume multiplication of the form: W*x + b
 import numpy as np
+import tensorflow as tf
 
 # Do I want to create a new TF network? Or just go directly to .nnet?
 # I think I want to condense to a new tf network, because then I can run some test inputs through both new and old and make sure they're the same...
@@ -16,24 +17,149 @@ import numpy as np
 # hm but counting the number of times it's called...isn't a...clean...choice
 # I think I should aim for having a single relu activation each layer
 
-def are_inputs_vars(op):
-	return [is_variable(i) for i in op.inputs]
 
-# maybe keep a list of weights and biases through the recursion?
+def are_inputs_vars(ops):	
+	return [[is_variable(i) for i in op.inputs] for op in ops]
+
+# stack matrices for different inputs
+def matrix_stacker(mats):
+	height = sum([m[0].shape[0] for m in mats])
+	width = sum([m[0].shape[1] for m in mats])
+	# for stacking mats, sttart with zero matrix
+	# replace values fo specific pieces
+	mega_mat = np.zeros((height, width))
+	mega_bias = np.zeros((height, 1))
+	i = 0
+	j = 0
+	for mat in mats:
+		n = mat[0].shape[0]
+		m = mat[0].shape[1]
+		mega_mat[i:i+n,j:j+m] = mat[0]
+		mega_bias[i:i+n] = mat[1]
+		i=n
+		j=m
+	return mega_mat, mega_bias		
+
 # inputs: final op
 # outputs: list of weights and biases for a flattened representation of the network 
-def parse_network(op, temp_W, temp_b, final_W, final_b, activation_type):
-	if not (op.type==activation_type):
-		# if there is more than 1 variable input
-		if sum(are_inputs_vars(op)) > 1:
+def parse_network(ops, temp_W, temp_b, final_W, final_b, activation_type, sess):
+	if all([op.type not in [activation_type, 'Identity'] for op in ops]):
+		mats = [op_to_mat(op, sess) for op in ops]
+		# the line that is actually doing something interesting:
+		###########################################
+		mega_mat, mega_bias = matrix_stacker(mats)
+		###########################################
+		temp_W.append(mega_mat)
+		temp_b.append(mega_bias)
+		# get input ops to recurse on
+		all_inputs = [op.inputs for op in ops]
+		iops = [item.op for sublist in all_inputs for item in sublist]
+		iops_inds = [is_variable(item) for sublist in all_inputs for item in sublist]
+		var_iops = []
+		# true-false indexing to get input ops that correspond to variables
+		for i in range(len(iops)):
+			if iops_inds[i]:
+				var_iops.append(iops[i])
+		## HANDLE DUPLICATES
+		return parse_network(var_iops, temp_W, temp_b, final_W, final_b, activation_type, sess)
 
-		else: # if there is only 1 variable input
-
-	else # if op is of type activation
+	elif all([op.type=='Identity' for op in ops]): 
+		# all ops are of type "variable read"
+		# this means we've gotten back to the beginning
+		## HANDLE DUPLICATES
+		if len(temp_W) > 0:
+			W, b = condense_list(temp_W, temp_b)
+			final_W.append(W)
+			final_b.append(b)
+		return final_W, final_b ## woo!!!! :D
+	elif all([op.type==activation_type for op in ops]): 
+		# if all ops are of type activation
 		# multiply temporary tensor list togeter to produce final tensor. add the final tensor list
 		# record activation type? into activation list?
 		# potentially record into .nnet file?
 		# recurse on input
+		# BUT if temp list is empty, this means we are just STARTING a squish-tensor-phase
+		pass
+	else: # there is a mixture of real ops, variable reads, and/or activations
+		# THENN add in "identity" ops for the variable reads and the activations until the real ops are "drawn down"
+		# TODO next: write the above elif
+		import pdb; pdb.set_trace()
+		mats = []
+		for op in ops:
+			if op.type not in ['Identity', activation_type]:
+				mats.append(op_to_mat(op,sess))
+			else: 
+				mats.append(get_identity_mat(op, activation_type))
+		mega_mat, mega_bias = matrix_stacker(mats)
+		temp_W.append(mega_mat)
+		temp_b.append(mega_bias)
+		# get inputs to recurse on (but not for activations and variable reads)
+		var_iops = []
+		for op in ops:
+			if op.type not in ['Identity', activation_type]:
+				for oi in op.inputs:
+					if is_variable(oi):
+						var_iops.append(oi.op)
+			else:
+				var_iops.append(op)
+		## HANDLE DUPLICATES
+		return parse_network(var_iops, temp_W, temp_b, final_W, final_b, activation_type, sess)
+
+# usage:
+# s = set(op_inputs)
+# if len(s) < len(op_inputs):
+# 		W,b = handle_duplicates(op_inputs)
+# 		temp_W.append(W)
+# 		temp_b.append(b)
+# # ELSE: continue as normal
+def handle_duplicates(op_inputs):
+	import pdb; pdb.set_trace()
+	d = {}
+	letter = 'A'
+	for oi in op_inputs:
+		if oi in d.keys():
+			pass
+		else: 
+			d[oi] = letter
+			letter = get_next_letter(letter)
+	# ending point:
+	labeled_ois = [d[oi] for oi in op_inputs]
+	print("labeled_inputs: ", labeled_ois)
+	# starting point
+	# create unique list of inputs
+	s = set(op_inputs)
+	n = len(s)
+	unique_ois = [s.pop() for i in range(n)]
+	unique_labeled_ois = [d[oi] for oi in unique_ois]
+	print("set of inputs: ", unique_labeled_ois)
+	# create matrix taking us from starting point to ending point
+	m = np.chararray((len(op_inputs), n))
+	m[:]='0'
+	# write one row at a time
+	# the index where the 'I' goes is the index in the set ("starting point") of the character in the ending point
+	rows = len(op_inputs)
+	for r in range(rows):
+		ind = unique_labeled_ois.index(labeled_ois[r])
+		m[r,ind] = 'I'
+	print("conversion matrix: ", m)
+
+	# now convert back to numbers
+	
+	return m
+
+def get_next_letter(l):
+	if l[-1] == 'Z':
+		new_l = 'A'*(len(l)+1)
+	else:
+		n = ord(l[-1])
+		new_l = l[0:-1]+chr(n+1) 
+	return new_l
+
+def get_identity_mat(op, activation_type):
+	n = op.outputs[0].shape[0].value
+	W = np.eye(n)
+	b = np.zeros([n,1])
+	return W,b
 
 # find dim of vector that is not 1
 # assume 2D only
@@ -84,7 +210,7 @@ def get_matmul_mat(op,sess):
 	elif is_variable(op.inputs[0]) and is_variable(op.inputs[1]):
 		raise ValueError('Both inputs to matmul are variables')
 	else: # both are constants
-		rause ValueError('Both inpust to matmul are constants. Do you want to implement this?')
+		raise ValueError('Both inputs to matmul are constants. Do you want to implement this?')
 	return (W,b)
 
 # inputs: op
@@ -92,12 +218,12 @@ def get_matmul_mat(op,sess):
 def op_to_mat(op, sess):
 	if op.type == 'Add':
 		W,b = get_add_mat(op, sess)
-	elif op.type == 'MatMul'
+	elif op.type == 'MatMul':
 		W,b = get_matmul_mat(op, sess)
 	else:
 		print('op type:', op.type)
 		raise ValueError('op type not supported')
-	return (tf.constant(W), tf.constant(b))
+	return W,b
 
 
 # return true if is derived of a variable
--- simple_parsing.py
+++ simple_parsing.py
@@ -1,22 +1,47 @@
 import tensorflow as tf
-x = tf.Variable([[0.0], [0.0]])
-y = tf.Variable([[1.0], [1.0]])
-W1 = tf.constant([[2.0, 0.0],[0.0, 2.0]])
-b1 = tf.constant([[-1.0], [-1.0]])
-W2 = -W1
-b2 = -b1
-with tf.name_scope("matmul_1"):
-	z1 = W1@x + b1
+import numpy as np
 
+def graph1():
+	x = tf.Variable([[0.0], [0.0]])
+	y = tf.Variable([[1.0], [1.0]])
+	W1 = tf.constant([[2.0, 0.0],[0.0, 2.0]])
+	b1 = tf.constant([[-1.0], [-1.0]])
+	W2 = -W1
+	b2 = -b1
+	with tf.name_scope("matmul_1"):
+		z1 = W1@x + b1
+	with tf.name_scope("matmul_2"):
+		z2 = W2@y + b2
+	return z1,z2
 
-with tf.name_scope("matmul_2"):
-	z2 = W2@y + b2
+R = tf.Variable([[1.0],[1.0]])
+C = tf.constant([[-1.0], [-1.0]])
+W1 = tf.constant(2*np.eye(2), dtype='float32')
+b1 = tf.constant([[5.0], [5.0]])
+W2 = tf.constant(3*np.eye(2), dtype='float32')
+b2 = tf.constant([[7.0], [7.0]])
 
-	
+Q = R + C
+x= W1@Q + b1
+y = W2@R + b2
+F = x + y
+
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
-LOGDIR = "/Users/Chelsea/Dropbox/AAHAA/src/OverApprox/tensorboard_logs/simple_parsing"
-train_writer = tf.summary.FileWriter(LOGDIR)
-train_writer.add_graph(sess.graph)
-train_writer.close()
+def write(sess):
+	LOGDIR = "/Users/Chelsea/Dropbox/AAHAA/src/OverApprox/tensorboard_logs/simple_parsing"
+	train_writer = tf.summary.FileWriter(LOGDIR)
+	train_writer.add_graph(sess.graph)
+	train_writer.close()
+
+import parsing as p
+
+p.parse_network([F.op], [], [], [], [], tf.nn.relu, sess)
+
+
+
+
+
+
+
