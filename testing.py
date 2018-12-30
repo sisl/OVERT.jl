e9f8b3a4c7401879e193dd95bad280452590e434
Chelsea Sidrane <csidrane@stanford.edu>
2018-12-29 19:08:45 -0800

    added tests for condense_lsit function

---
+++ testing.py
@@ -0,0 +1,25 @@
+# more testing code
+import numpy as np
+
+# test: condense_list
+W = []
+b = []
+for i in range(2):
+	W.append(np.random.rand(2,2))
+	b.append(np.random.rand(2,1))
+
+x = np.array([[1.0],[1.0]])
+
+# hand multiply:
+y = x
+for i in range(2):
+	y = W[i]@y + b[i]
+
+import parsing as p
+
+Wc, bc = p.condense_list(W,b)
+yc = Wc@x + bc
+
+print("yc: ", yc)
+print("y: ", y)
+#assert(all(yc == y))
