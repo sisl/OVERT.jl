a52275ad1086a1a3bf32fb44300a8c92bee5608e
Chelsea Sidrane <csidrane@stanford.edu>
2019-01-25 16:54:28 -0800

    more progress

--- correct_overapprox.py
+++ correct_overapprox.py
@@ -74,8 +74,8 @@ class ReluProtector():
     def __init__(self, state_dim):
         self.before = tf.constant(np.vstack([np.eye(state_dim), -np.eye(state_dim)]), dtype='float32')
         self.after = tf.constant(np.hstack([np.eye(state_dim), -np.eye(state_dim)]), dtype='float32')
-    def apply(self, state):
-        return self.after@(tf.nn.relu(self.before@state))
+    def apply(self, state, name=""):
+        return tf.matmul(self.after, tf.nn.relu(self.before@state), name=name)
 
 def build_multi_step_network(theta_0, theta_dot_0, controller, dynamics, nsteps, ncontroller_act):
     with tf.name_scope("assign_init_vals"):
@@ -99,12 +99,12 @@ def build_multi_step_network(theta_0, theta_dot_0, controller, dynamics, nsteps,
         #
         # apply relu protection
         with tf.name_scope("relu_protection"):
-            for i in range(ncontroller_act):
-                theta = relu_protector.apply(theta)
-                theta_dot = relu_protector.apply(theta_dot)
-                theta_dot_LBs = [relu_protector.apply(tdlb) for tdlb in theta_dot_LBs]
-                theta_dot_UBs = [relu_protector.apply(tdub) for tdub in theta_dot_UBs]
-                theta_dot_hats = [relu_protector.apply(tdh) for tdh in theta_dot_hats]
+            for j in range(ncontroller_act):
+                theta = relu_protector.apply(theta, name="theta")
+                theta_dot = relu_protector.apply(theta_dot, name="theta_dot")
+                theta_dot_LBs = [relu_protector.apply(tdlb, name="tdlb") for tdlb in theta_dot_LBs]
+                theta_dot_UBs = [relu_protector.apply(tdub, name="tdub") for tdub in theta_dot_UBs]
+                theta_dot_hats = [relu_protector.apply(tdh, name="tdh") for tdh in theta_dot_hats]
             
         # input of dynamics is torque(action), output is theta theta-dot at the next timestep
         with tf.name_scope("run_dynamics"):
@@ -154,22 +154,71 @@ display_ops(sess)
 feed_dict = {
         theta_0: init_theta,
         theta_dot_0: init_theta_dot,
-        'assign_init_vals/theta_dot_hat_1:0':np.array([[0.0]]),
-        'assign_init_vals/theta_dot_hat_2:0':np.array([[0.0]]),
-        'assign_init_vals/theta_dot_hat_3:0':np.array([[0.0]]),
+        'assign_init_vals/theta_dot_hat_1:0':np.array([[0.1]]),
+        'assign_init_vals/theta_dot_hat_2:0':np.array([[0.2]]),
+        'assign_init_vals/theta_dot_hat_3:0':np.array([[0.3]]),
     }
 sess.run(tf.global_variables_initializer())
-theta, theta_dot, theta_dot_hats, theta_dot_LBs, theta_dot_UBs = sess.run([theta, theta_dot, theta_dot_hats, theta_dot_LBs, theta_dot_UBs], feed_dict=feed_dict)
-print("theta: ", theta)
-print("theta_dot: ", theta_dot)
-print("theta_dot_hats: ", theta_dot_hats)
-print("theta_dot_LBs: ", theta_dot_LBs)
-print("theta_dot_UBs: ", theta_dot_UBs)
+theta_v, theta_dot_v, theta_dot_hats_v, theta_dot_LBs_v, theta_dot_UBs_v = sess.run([theta, theta_dot, theta_dot_hats, theta_dot_LBs, theta_dot_UBs], feed_dict=feed_dict)
+print("theta: ", theta_v)
+print("theta_dot: ", theta_dot_v)
+print("theta_dot_hats: ", theta_dot_hats_v)
+print("theta_dot_LBs: ", theta_dot_LBs_v)
+print("theta_dot_UBs: ", theta_dot_UBs_v)
+
+# print output op names
+print("theta, theta_dot")
+print([t.op.name for t in [theta, theta_dot]])
+print("theta_dot_hats")
+print([t.op.name for t in theta_dot_hats])
+print("theta_dot_LBs")
+print([t.op.name for t in theta_dot_LBs])
+print("theta_dot_UBs")
+print([t.op.name for t in theta_dot_UBs])
+
 
 # write to tensorboard
 LOGDIR = "/Users/Chelsea/Dropbox/AAHAA/src/OverApprox/tensorboard_logs/new_approach_"+f_id
 write_to_tensorboard(LOGDIR, sess)
 # next run at command line, e.g.:  tensorboard --logdir=/Users/Chelsea/Dropbox/AAHAA/src/OverApprox/tensorboard_logs/UGH_multi_2
+
+# parse and eval and turn into .nnet and set up inequalities in NV.jl
+# first convert variables to constants
+output_node_names = ['run_dynamics_2/Dynamics_3/theta_3', # theta
+                    'relu_protection_2/tdh_3', 
+                    'relu_protection_2/tdh_4', 
+                    'relu_protection_2/tdh_5', # theta dots
+                    'relu_protection_2/tdlb_2', 
+                    'relu_protection_2/tdlb_3', 
+                    'run_dynamics_2/Dynamics_3/theta_dot_LB_3', # theta dot LBs
+                    'relu_protection_2/tdub_2', 
+                    'relu_protection_2/tdub_3', 
+                    'run_dynamics_2/Dynamics_3/theta_dot_UB_3', # theta dot UBs
+                    ]
+output_graph_def = graph_util.convert_variables_to_constants(
+        sess, # sess used to retrieve weights
+        sess.graph.as_graph_def(), # graph def used to retrieve nodes
+        output_node_names # output node names used to select useful nodes
+        )
+# print op list to make sure its only stuff we can handle
+print("op set: ", {(x.op,) for x in output_graph_def.node})
+
+# turn graph def back into graph
+tf.import_graph_def(output_graph_def)
+display_ops(sess)
+g = tf.get_default_graph()
+
+# get output ops
+output_ops = [g.get_operation_by_name("import/"+node) for node in output_node_names]
+
+activation_type = 'Relu'
+W,b = parsing.parse_network_wrapper(output_ops, activation_type, sess)
+# order of input ops: 
+# ['import/initial_values/theta_dot_0', 'import/assign_init_vals/theta_dot_hat_1', 'import/initial_values/theta_0', 'import/assign_init_vals/theta_dot_hat_2', 'import/assign_init_vals/theta_dot_hat_3']
+
+
+
+
 
 
 
