ec8ffc581f8bb222394bbcbde50bd0af2f811302
Joe Vincent <joevincentkc@gmail.com>
2019-08-18 13:31:21 -0700

    cleaned up code and added state bypass

--- rnn2ffnn.jl
+++ rnn2ffnn.jl
@@ -70,15 +70,16 @@ Base.:+(x::Number, y::AbstractArray) = x .+ y
 
 
 
+
+
 ################################################################################
 # This is the RNN we want to convert to FFNN.
 rnn = Chain(Dense(2, 4, relu), RNN(4, 4, relu), Dense(4, 1, identity))
 
 # Create the FFNN from the given RNN model. Currently the list "new" is hardcoded
-function new_net(model)
-    W, b = new_params(model)
-    # print("\n\nout.layers[1]:\n", size(W[1]), "\n")
-    bypass = bypass_nodes(model)
+function new_net(model, state_size)
+    W, b = new_params(model, state_size)
+    bypass = bypass_nodes(model, state_size); #bypass = bypass .+ state_size
     # new = [Dense(W[i], b[i], ReLUBypass(bypass[i])) for i=1:num_layers(model)-2]
     # new = append!(new, [Dense(W[end], b[end], identity)])
     # print("\n\nNEW: \n", new, "\n")
@@ -87,109 +88,84 @@ function new_net(model)
 end
 
 # Returns weights and biases of new FFNN based on input RNN
-function new_params(model)
+function new_params(model, state_size)
+    W_s = Matrix(1I, state_size, state_size)
+    b_s = zeros(state_size)
     sizes_layers = layer_sizes(model)
     rnn_index = rnn_layer(model)
-    size_latent = sizes_layers[rnn_layer(model)+1]
+    size_latent = size(model[rnn_layer(model)].init)[1]
     weights = Vector{Array{Float64}}(undef, num_layers(model)-1)
     biases  = Vector{Array{Float64}}(undef, num_layers(model)-1)
     for i = 1:num_layers(model)-1
         if i < rnn_index
             weights[i] = hcat(zeros(sizes_layers[i+1], size_latent), Tracker.data(model.layers[i].W))
             weights[i] = vcat(Matrix(1I, size_latent, sizes_layers[i]+size_latent), weights[i])
-            biases[i]  = vcat(zeros(size_latent), Tracker.data(model.layers[i].b))
+            weights[i] = hcat(zeros(size(weights[i])[1], state_size), weights[i])
+            weights[i] = vcat(Matrix(1I, state_size, size(weights[i])[2]), weights[i])
+            biases[i]  = vcat(zeros(size_latent+state_size), Tracker.data(model.layers[i].b))
         elseif i == rnn_index
             weights[i] = vcat(hcat(Tracker.data(model.layers[i].cell.Wh), Tracker.data(model.layers[i].cell.Wi)), hcat(Tracker.data(model.layers[i].cell.Wh), Tracker.data(model.layers[i].cell.Wi)))
+            weights[i] = hcat(zeros(size(weights[i])[1], state_size), weights[i])
+            weights[i] = vcat(Matrix(1I, state_size, size(weights[i])[2]), weights[i])
             biases[i]  = vcat(Tracker.data(model.layers[i].cell.h) + Tracker.data(model.layers[i].cell.b), Tracker.data(model.layers[i].cell.h) + Tracker.data(model.layers[i].cell.b))
+            biases[i]  = vcat(zeros(state_size), biases[i])
         elseif i > rnn_index
             weights[i] = hcat(zeros(sizes_layers[i+1], size_latent), Tracker.data(model.layers[i].W))
             weights[i] = vcat(weights[i], Matrix(1I, size_latent, sizes_layers[i]+size_latent))
-            biases[i]  = vcat(Tracker.data(model.layers[i].b), zeros(size_latent))
+            weights[i] = hcat(zeros(size(weights[i])[1], state_size), weights[i])
+            weights[i] = vcat(Matrix(1I, state_size, size(weights[i])[2]), weights[i])
+            biases[i]  = vcat(zeros(state_size), Tracker.data(model.layers[i].b), zeros(size_latent))
         end
     end
     return weights, biases
 end
 
-# Returns number of layers of a model, including input layer
-function num_layers(model)
-    count = 0
-    for layer in model.layers
-        count += 1
-    end
-    count + 1  # +1 to account for input layer
-end
-
 # Returns which indices of each layer to ReLUBypass
-function bypass_nodes(model)
-    sizes_layers = layer_sizes(model)
-    size_latent = sizes_layers[rnn_layer(model)+1]
-    bypass = Vector{Vector{Int64}}(undef, num_layers(model)-1)
-    rnn_index = rnn_layer(model)
-    for i = 1:num_layers(model)-1
-        if i < rnn_index
-            bypass[i] = collect(1:size_latent)
-        elseif i == rnn_index
-            bypass[i] = []
-        elseif i > rnn_index
-            bypass[i] = collect(1:size_latent)
-        end
-    end
-    bypass
+function bypass_nodes(model, state_size)
+    size_latent = size(model[rnn_layer(model)].init)[1]
+    [i == rnn_layer(model) ? collect(1:state_size) : collect(1:size_latent+state_size) for i = 1:num_layers(model)-1]
 end
 
-# Return list of size of each layer in model. Input layer included.
-function layer_sizes(model)
-    sizes_layers = zeros(num_layers(model))
-    sizes_layers[1] = size(model[1].W)[2]
-    i = 1
-    for layer in model.layers
-        if layer isa Flux.Dense
-            sizes_layers[i+1] = size(model[i].W)[1]
-        elseif layer isa Flux.Recur
-            sizes_layers[i+1] = size(model[i].init)[1]
-        end
-        i += 1
-    end
-    Int.(sizes_layers)
-end
+# Returns number of layers of a model, including input layer
+num_layers(model) = 1 + sum([1 for l in model.layers])
+
+# Return list of size of each layer in flux model. Input layer included.
+layer_sizes(x) = append!([size(x[1].W)[2]], [x.layers[i] isa Dense ? size(x[i].W)[1] : size(x[i].init)[1] for i=1:num_layers(x)-1])
 
 # Return RNN Layer. (Input limited to one RNN layer)
-function rnn_layer(model)
-    index = 0
-    i = 1
-    for layer in model.layers
-        if layer isa Flux.Recur
-            index = i
-        end
-        i+=1
-    end
-    index
-end
+rnn_layer(model) = findfirst(l->l isa Flux.Recur, model.layers)
 
-# Creates a network where the input is ReLUBypassed for a given number of layers
-function state_bypass(state_size, layer_count)
-    layer_list = fill(Dense(Matrix(1I, state_size, state_size), zeros(state_size), ReLUBypass(collect(1:state_size)...)), (layer_count))
-    out = relu_bypass(Chain(layer_list...))
-end
+
+
 
 ## CHECK CORRECTNESS ##
-size_latent = layer_sizes(rnn)[rnn_layer(rnn)+1]
-input = rand(2)
+size_latent = size(rnn[rnn_layer(rnn)].init)[1]
+state = [1.1, -11.5]; state_size = size(state)[1]
+input = 2rand(2) - 1.0
 eval_point = input
-eval_point2 = vcat(zeros(size_latent), input)
+eval_point2 = vcat(state, zeros(size_latent), input)
 
-ffnn = new_net(rnn)
-ans_old = rnn(eval_point)
-ans_new = ffnn(eval_point2)
+ffnn = new_net(rnn, state_size)
+ans_old = Tracker.data(rnn(eval_point))
+ans_new = Tracker.data(ffnn(eval_point2))
 print("\nCheck for same control output and latent state:")
-print("\nRNN: ", Tracker.data(ans_old[1]), Tracker.data(rnn.layers[rnn_layer(rnn)].state))
-print("\nFFNN: ", ans_new)
+print("\nRNN: ", ans_old[1], Tracker.data(rnn.layers[rnn_layer(rnn)].state))
+print("\nFFNN: ", ans_new)  # Outputs [theta, theta_dot, u, l1, l2, l3, l4] l1 is first value of latent state
 # ans_old[1] == ans_new[1] ? print("\nMatching Control Output✅") : print("\nNot Matching Control Output ❌")
 # model.layers[2].state == ans_new[2:end] ? print("\nMatching RNN State ✅") : print("\nNot Matching RNN State ❌")
 
 
 
 
+
+
+
+## EXTRANEOUS FUNCTIONS ##
+# # Creates a network where the input is ReLUBypassed for a given number of layers
+# function state_bypass(state_size, layer_count)
+#     layer_list = fill(Dense(Matrix(1I, state_size, state_size), zeros(state_size), ReLUBypass(collect(1:state_size)...)), (layer_count))
+#     out = relu_bypass(Chain(layer_list...))
+# end
 
 ## EXTRANEOUS PRINTING OF INPUT RNN ##
 # # print weights #
