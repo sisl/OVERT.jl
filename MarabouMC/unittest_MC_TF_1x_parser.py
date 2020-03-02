70bbded82679ea3a94e1d9f71a0e466f5dd15264
Chelsea Sidrane <csidrane@stanford.edu>
2020-03-02 14:40:03 -0800

    support for live sessions

--- MarabouMC/unittest_MC_TF_1x_parser.py
+++ MarabouMC/unittest_MC_TF_1x_parser.py
@@ -12,19 +12,30 @@ from MC_TF_parser import TFConstraint
 import os
 import colored_traceback.always
 
+write_to_file = False # change this to test different things
+
 # create network
 sess = tf.Session()
 with sess.as_default():
     x = tf.placeholder(shape=(2,1), dtype='float64')
-    W = np.random.rand(1,2)
-    b = np.random.rand(1,1)
+    W = tf.Variable(np.random.rand(1,2))
+    b = tf.Variable(np.random.rand(1,1))
     output = tf.nn.relu(tf.matmul(W,x) + b)
+    sess.run(tf.global_variables_initializer()) # actually sets Variable values to values specified
 
 # write graph
 filename = write_graphdef(sess, output)
 
 # read graphdef
-tfconstraints = TFConstraint(filename, inputNames=[x.op.name], outputName=output.op.name)
+if write_to_file:
+    tfconstraints = TFConstraint(filename, inputNames=[x.op.name], outputName=output.op.name)
+else:
+    # smoosh all Variables to Constants, put into new graph
+    new_graph_def = tf.compat.v1.graph_util.convert_variables_to_constants(sess, sess.graph.as_graph_def(), [output.op.name])
+    new_graph = tf.Graph()
+    with new_graph.as_default():
+        tf.import_graph_def(new_graph_def, name="")
+    tfconstraints = TFConstraint(sess=tf.Session(graph=new_graph), inputNames=[x.op.name], outputName=output.op.name)
 
 # test that output of tf network satisfies constraints of TFConstraint
 # pick random inputs, run with tf graph, get outputs
@@ -42,4 +53,5 @@ print(tfsol)
 
 # assert that they are the same!
 assert(all(np.isclose(output_sols, tfsol)))
+print("Weee test passes!!!")
 
