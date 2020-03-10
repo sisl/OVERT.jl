2a39e613cfeecc31c2c1272acd8b86362849ea80
Ahmed Irfan <irfan@cs.stanford.edu>
2020-03-10 13:44:26 -0700

    comment

--- MarabouMC/properties.py
+++ MarabouMC/properties.py
@@ -30,6 +30,7 @@ class ConstraintProperty():
         # for now, only support single simple constraint
         assert(len(self.constraints) == 1)
         c = self.constraints[0]
+        # perhaps we should implement the complement method in the constraint class
         if c.type == ConstraintType('GREATER'):
             ccomp = Constraint(ConstraintType('LESS_EQ'))
         elif c.type == ConstraintType('LESS'):
