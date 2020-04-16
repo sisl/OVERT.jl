e090df85919aed9f2c05f1abb4788f625d45381e
Chelsea Sidrane <csidrane@stanford.edu>
2020-04-15 17:36:19 -0700

    major changes including implementation for MatrixConstraint, conjunction of constraints, and more

--- MarabouMC/check_overapprox.py
+++ MarabouMC/check_overapprox.py
@@ -1,5 +1,5 @@
 from MC_constraints import Constraint, MatrixConstraint, MaxConstraint, ReluConstraint
-
+import numpy as np
 class OverapproxVerifier:
     """
     A function to verify the validity of an overapproximation using dreal. 
@@ -8,7 +8,8 @@ class OverapproxVerifier:
     def __init__(self, phi, phihat):
         self.phi = phi # the true function
         self.phihat = phihat # the overapproximation
-        self.smtlib = [] # the formula in smtlib2 
+        self.smtlib_formula = [] # the formula not(phi => phihat) in smtlib2
+        self.formula_converter = FormulaConverter() 
 
     def check(self):
         self.convert_formula()
@@ -51,7 +52,10 @@ class FormulaConverter:
     
     def define_atom(self, atomname, atomvalue):
         eq_expr = self.prefix_notate("=", [atomname, atomvalue])
-        return self.prefix_notate("assert", [eq_expr])
+        return self.assert_statement(eq_expr)
+    
+    def assert_statement(self, expr):
+        return self.prefix_notate("assert", [expr])
     
     def negate(self, expr):
         return self.prefix_notate("not", [expr])
@@ -71,17 +75,35 @@ class FormulaConverter:
     def define_relu(self):
         return "(define-fun relu ((x Real)) Real (max x 0))"
     
-    def assert_conjunction(self, constraint_list):
-        # conjunct = []
-        # for item in constraint_list:
-        #     # convert somehow
-        #     # then
-        #     pass
-        # return conjunct
-        pass
+    def assert_conjunction(self, constraint_list, conjunct_name=None):
+        formula, conjunct_name = self.declare_conjunction(constraint_list, conjunct_name=conjunct_name) # declare conjunction
+        formula += [self.assert_statement(conjunct_name)] # assert conjunction
+        return formula
 
     def assert_negated_conjunct(self, constraint_list):
         pass
+
+    def declare_conjunction(self, constraint_list, conjunct_name=None):
+        """
+        Given a list of constraints, declare their conjunction but DO NOT
+        assert their conjunction.
+        e.g. 
+        (declare ... A)
+        (assert A = (<= y 5))
+        (declare ... B)
+        (assert B = (>= z 6))
+        ...
+        (declare ... phi)
+        (assert (= phi (and A B)))
+        But notice we are just _defining_ phi, we are not asserting that
+        phi _holds_, which would be: (assert phi) [not doing that tho!]
+        """
+        bool_var_defs, bool_var_names = self.declare_list(constraint_list)
+        if conjunct_name is None:
+            conjunct_name = self.get_new_var()
+        conjunct = self.prefix_notate("and", bool_var_names)
+        formula = bool_var_defs + [self.define_atom(conjunct_name, conjunct)]
+        return formula, conjunct_name
 
     def declare_list(self, constraint_list):
         """
@@ -91,22 +113,25 @@ class FormulaConverter:
         (assert A = (< y 5))
         But DON'T assert either true or false for the atom A
         """
-        statements = []
-        for item in constraint_list:
-            bool_var = self.get_new_var()
-            statements += [self.declare_const(bool_var, "Bool")]
+        bool_var_defs = [] # definitions + declarations
+        bool_var_names = [] # names
+        for item in constraint_list:  
             if isinstance(item, Constraint):
                 expr = self.convert_Constraint(item)
             elif isinstance(item, MatrixConstraint):
                 expr = self.convert_MatrixConstraint(item)
-            elif isinstance(item, MaxConstraint):
-                expr = self.convert_MaxConstraint(item)
             elif isinstance(item, ReluConstraint):
                 expr = self.convert_ReluConstraint(item)
+            elif isinstance(item, MaxConstraint):
+                expr = self.convert_MaxConstraint(item)
             else:
                 raise NotImplementedError
-            statements += [self.define_atom(bool_var, expr)]
-        return statements
+            for e in expr:
+                bool_var = self.get_new_var()
+                bool_var_names += [bool_var]
+                bool_var_defs += [self.declare_const(bool_var, "Bool")]
+                bool_var_defs += [self.define_atom(bool_var, e)]
+        return bool_var_defs, bool_var_names
 
     def convert_Constraint(self, c):
         """
@@ -119,30 +144,41 @@ class FormulaConverter:
         (<= (+ (* 5 y) (* -6 y)) 7)
         # + can take any number of args BUT unary minus must be handled separately: (- 5)
         """
+        coeffs = [m.coeff for m in c.monomials]
+        variables = [m.var for m in c.monomials]
+        return self.convert_constraint_helper(coeffs, variables, c.type.__repr__(), c.scalar)
+    
+    def convert_constraint_helper(self, coeffs, variables, eq_type, scalar):
         # build up list of monomials: (* 5 y), (* (- 6) x), applying unary minus where necessary
         m_list = []
-        for m in c.monomials:
-            if m.coeff < 0:
+        for i in range(len(variables)):
+            if coeffs[i] < 0:
                 # apply unary minus
-                smtlib_coeff = self.prefix_notate("-", [abs(m.coeff)])
+                smtlib_coeff = self.prefix_notate("-", [abs(coeffs[i])])
             else:
-                smtlib_coeff = m.coeff
-            m_list += self.prefix_notate("*", [smtlib_coeff, m.var])
+                smtlib_coeff = coeffs[i]
+            m_list += [self.prefix_notate("*", [smtlib_coeff, variables[i]])]
         # add monomials together
         left_side = self.prefix_notate("+", m_list)
         # construct right side
-        if c.scalar < 0:
-            right_side = self.prefix_notate("-", [abs(c.scalar)])
+        if scalar < 0:
+            right_side = self.prefix_notate("-", [abs(scalar)])
         else:
-            right_side = c.scalar
+            right_side = scalar
         # construct (R (monomials) scalar)
-        return self.prefix_notate(c.type.__repr__(), [left_side, right_side])
+        return [self.prefix_notate(eq_type, [left_side, right_side])]
         
-    def convert_MatrixConstraint(self):
+    def convert_MatrixConstraint(self, c: MatrixConstraint):
         """
-        Convert a constraint of type MatrixConstraint
+        Convert a constraint of type MatrixConstraint.
+        Ax = b 
+        to a list of 1D constraints in smtlib
         """
-        pass
+        constraints = []
+        m = c.A.shape[0]
+        for row in range(m):
+            constraints += self.convert_constraint_helper(c.A[row,:], c.x.flatten(), c.type.__repr__(), c.b[row][0])
+        return constraints
 
     def convert_ReluConstraint(self, c):
         """
@@ -152,33 +188,24 @@ class FormulaConverter:
         to:
         (= (relu x) y)
         """
-        try: # handle multi dimensional inputs
-            l = []
-            for i in range(len(c.varin)):
-                left_side = self.prefix_notate("relu", [c.varin[i]])
-                right_side = c.varout[i]
-                l += [self.prefix_notate("=", [left_side, right_side])]
-            return l
-        except: # single dimensional inputs
-            left_side = self.prefix_notate("relu", c.varin)
-            right_side = c.varout
-            return self.prefix_notate("=", [left_side, right_side])
+        varsin = np.array(c.varin, dtype='object').flatten() # allows handling multi-dimensional inputs
+        varsout = np.array(c.varout, dtype='object').flatten()
+        l = []
+        for i in range(len(varsin)):
+            left_side = self.prefix_notate("relu", [varsin[i]])
+            right_side = varsout[i]
+            l += [self.prefix_notate("=", [left_side, right_side])]
+        return l
         
     def convert_MaxConstraint(self, c):
         """
         Convert a constraint of type MaxConstraint
         """
-        try: # handle multi dimensional inputs
-            l = []
-            for i in range(len(c.varin)):
-                left_side = self.prefix_notate("max", [c.var1in[i], c.var2in[i]])
-                right_side = c.varout[i]
-                l += [self.prefix_notate("=", [left_side, right_side])]
-            return l
-        except: # single dimensional inputs
-            left_side = self.prefix_notate("max", [c.var1in, c.var2in])
-            right_side = c.varout
-            return self.prefix_notate("=", [left_side, right_side])
+        # does NOT handle multi dimensional inputs
+        assert(not isinstance(c.var1in, np.ndarray) and not isinstance(c.var1in, list))
+        left_side = self.prefix_notate("max", [c.var1in, c.var2in])
+        right_side = c.varout
+        return [self.prefix_notate("=", [left_side, right_side])]
 
     def get_new_var(self):
         self.var_count += 1
--- MarabouMC/unittest_check_overapprox.py
+++ MarabouMC/unittest_check_overapprox.py
@@ -1,5 +1,6 @@
+import numpy as np
 from check_overapprox import FormulaConverter
-from MC_constraints import Constraint, Monomial
+from MC_constraints import Constraint, Monomial, MaxConstraint, ReluConstraint, MatrixConstraint
 
 f = FormulaConverter()
 
@@ -11,8 +12,19 @@ print(f.define_atom("A", "(< y 5)"))
 
 print(f.negate("(< y 5)"))
 
-c = Constraint('LESS_EQ', [Monomial(-6, "x"), Monomial(5, "y")], -2)
-print(f.convert_Constraint(c))
+c1 = Constraint('LESS_EQ', [Monomial(-6, "x"), Monomial(5, "y")], -2)
+print(f.convert_Constraint(c1))
 
-print(f.declare_list([c, c]))
+c2 = MaxConstraint(['v1','v2'], 'v3')
+print(f.convert_MaxConstraint(c2))
+
+c3 = ReluConstraint('p', 'q')
+print(f.convert_ReluConstraint(c3))
+
+c4 = MatrixConstraint('EQUALITY', A=np.random.rand(2,2), x=np.array([['x'],['y']], dtype='object'), b=np.zeros((2,1)) )
+print(f.convert_MatrixConstraint(c4))
+
+print(f.declare_list([c1, c1, c2, c2, c3, c3, c4]))
+
+print('\n'.join(f.declare_conjunction([c1, c2, c3, c4])[0]))
 
