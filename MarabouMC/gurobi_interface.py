8a449dfc6eff75baa5d33a196356a7462f7c8bdc
Chelsea Sidrane <csidrane@stanford.edu>
2020-06-03 13:06:24 -0700

    implementing gurobi interface

--- MarabouMC/gurobi_interface.py
+++ MarabouMC/gurobi_interface.py
@@ -2,21 +2,23 @@ from MC_constraints import Constraint, ConstraintType, MatrixConstraint, ReluCon
 import numpy as np
 from MC_interface import Result
 import gurobipy as gp
+inf = gp.GRB.INFINITY 
 import pickle
 import os
+from MC_interface import substitute_c
 
 """
 Plan:
 - implement necessary high level functions for use with BMC:
     - clear - DONE
     - assert init set - DONE
-    - assert constraints
+    - assert constraints - DONE
     - check_sat
 
 - low level helper functions:
-    - get_new_var - DONE
-    - assert 1 constraint
-        - assert each type of constraint: relu, max, affine 1D, affine nD (matrix) 
+    - get_new_var(s) - DONE, DONE
+    - assert 1 constraint - DONE
+        - assert each type of constraint: relu - done, max - done, affine 1D - DONE, affine nD (matrix) - DONE
 
 - For relu and max, implement MIPverify encoding
     - go get encoding from paper
@@ -25,15 +27,23 @@ Plan:
     - data members:
         - gurobi model
         - varmap, BMC vars - > gurobi vars
+    - incorporate any bounds calculated by overt
+        - can init solver with dictionary -- if passed variable with same prefix...
+        (stripped of timing suffix like '@xx') then when doing "add var" in gurobi,
+        add bounds for this variable...? OH but...over time...bounds will change...
+        TABLE til figure out multi-step calls to overt.
+
 
 Useful references:
 https://www.gurobi.com/documentation/8.1/quickstart_mac/py_example_mip1_py.html
 
 """
 
-class GurobiPyWrapper()
-    def __init__(self):
+class GurobiPyWrapper():
+    def __init__(self, PWL_encoding='native'):
         self.clear()
+        # encoding for PWL disjunctions like max, relu
+        self.PWL_encoding = PWL_encoding # may be either 'native' or 'MIPverify'
     
     def clear(self):
         self.model = gp.Model("OVERT")
@@ -47,7 +57,7 @@ class GurobiPyWrapper()
         for k in init_set.keys():
             self.get_new_var(k, gp.GRB.CONTINUOUS, lb=init_set[k][0], ub=init_set[k][1])
 
-    def get_new_var(self, name, vtype, lb=-gp.GRB.INFINITY, ub = gp.GRB.INFINITY):
+    def get_new_var(self, name, vtype, lb=-inf, ub =inf):
         """
         type should be one of:
         GRB.CONTINUOUS, GRB.BINARY, GRB.INTEGER, GRB.SEMICONT, or GRB.SEMIINT
@@ -57,11 +67,93 @@ class GurobiPyWrapper()
             self.var_map[name] = gbv
         return self.var_map[name]
     
-    def assert_constraints(self, constraints):
-        pass
+    def handle_args(self, names, vtypes, lbs, ubs):
+        if lbs is None:
+            lbs = [-inf]*len(names)
+        if ubs is None:
+            ubs = [inf]*len(names)
+        if vtypes is None:
+            vtypes = [gp.GRB.CONTINUOUS]*len(names)
+        elif len(vtypes) == 1:
+            vtypes = vtypes*len(names)
+        return names, vtypes, lbs, ubs
 
-    def assert_1_constraint(self, c):
-        pass
+    def get_new_vars(self, names, vtypes=None, lbs=None, ubs=None):
+        names, vtypes, lbs, ubs = self.handle_args(names, vtypes, lbs, ubs)
+        new_vars = []
+        for i in range(len(names)):
+            new_vars.append(self.get_new_var(names[i], vtypes[i], lb=lbs[i], ub=ubs[i]))
+        #
+        if isinstance(names, np.ndarray):
+            return np.array(new_vars).reshape(names.shape)
+        else:
+            return new_vars    
+
+    def assert_constraints(self, constraints):
+        for c in constraints:
+            self.assert_one_constraint(c)
+
+    def assert_one_constraint(self, constraint):
+        if isinstance(constraint, MatrixConstraint):
+            self.assert_matrix_constraint(constraint)
+        elif isinstance(constraint, Constraint):
+            self.assert_simple_constraint(constraint)
+        elif isinstance(constraint, ReluConstraint):
+            self.assert_relu_constraint(constraint)
+        elif isinstance(constraint, MaxConstraint):
+            self.assert_max_constraint(constraint)
+        else:
+            # TODO: should min be implemented?
+            raise NotImplementedError
+    
+    def assert_matrix_constraint(self, c: MatrixConstraint):
+        # TODO: handle 'not equal' constraint (exception)
+        x = self.get_new_vars(c.x) # get gurobi vars
+        print("x shape is: ", c.x.shape, " b shape is: ", c.b.shape, " A shape is: ", c.A.shape)
+        self.matrix_helper(c.A, x, c.type, c.b)
+    
+    def assert_simple_constraint(self, c: Constraint):
+        # Note: if matrix interface is causing problems, 
+        # can add constraints by building up gurobi TempConstr one monomial at a time
+        # e.g. g1 = gurobi_x + gurobi_y,  g2 = g1 + 2*z ... etc.
+        A = np.array([m.coeff for m in c.monomials]).reshape(1,-1)
+        b = np.array([c.scalar]) #.reshape(1,1)
+        x = np.array(self.get_new_vars([m.var for m in c.monomials])).reshape(-1,1)
+        self.matrix_helper(A, x, c.type, b)
+    
+    def matrix_helper(self, A, x, R, b):
+        """
+        Assert a gurobi matrix constraint of a certain type R.
+        x should be GUROBI variables
+        R should be an instance of a ConstraintType
+        """
+        if R == ConstraintType('EQUALITY'):
+            self.model.addConstr(A @ x == b)
+        elif R == ConstraintType('LESS_EQ'):
+            self.model.addConstr(A @ x <= b)
+        elif R == ConstraintType('GREATER_EQ'):
+            self.model.addConstr(A @ x >= b)
+        else:
+            raise NotImplementedError
+
+    def assert_max(self, c: MaxConstraint):
+        varsin = self.get_new_vars([c.var1in, c.var2in])
+        varout = self.get_new_vars(c.varout)
+        if self.PWL_encoding == 'native':
+            self.model.addConstr(varout == max_(varsin))
+        elif self.PWL_encoding == 'MIPverify':
+            raise NotImplementedError
+        else:
+            raise NotImplementedError
+
+    def assert_relu(self, c: ReluConstraint):
+        if self.PWL_encoding == 'native':
+            max_version = MaxConstraint(varout=c.varout, varsin=[c.varin, 0])
+            self.assert_max(max_version)
+        elif self.PWL_encoding == 'MIPverify':
+            raise NotImplementedError
+        else:
+            raise NotImplementedError
 
 
 
