23236903c12d2f6eeaa8096ba2655ce0c2edb7f5
Amir <amir.maleki.zamenjani@gmail.com>
2020-06-19 05:34:44 -0700

    adding sherlock examples

--- examples/sherlock/example_tora.jl
+++ examples/sherlock/example_tora.jl
@@ -10,16 +10,18 @@ query = OvertQuery(
 	"nnet_files/sherlock/tora_bigger_controller_offset_10_scale_1.nnet", # network file
 	Id(),                                               # last layer activation layer Id()=linear, or ReLU()=relu
 	"MIP",                                              # query solver, "MIP" or "ReluPlex"
-	5,                                                 # ntime
-	0.1,                                                # dt
+	10,                                                 # ntime
+	1,                                                  # dt
 	-1,                                                 # N_overt
 	)
 
 input_set = Hyperrectangle(low=[0.6, -0.7, -0.4, 0.5], high=[0.7, -0.6, -0.3, 0.6])
 
-all_sets, all_sets_symbolic = symbolic_reachability(query, input_set)
+all_sets = many_timestep_concretization(query, input_set)[1]
+
+# all_sets, all_sets_symbolic = symbolic_reachability(query, input_set)
 output_sets, xvec, x0 = monte_carlo_simulate(query, input_set)
 
-fig = plot_output_sets(all_sets)
-fig = plot_output_sets([all_sets_symbolic]; linecolor=:red, fig=fig)
-fig = plot_output_hist(xvec, query.ntime; fig=fig, nbins=100)
+fig = plot_output_sets(all_sets; idx=[1,3])
+# fig = plot_output_sets([all_sets_symbolic]; linecolor=:red, fig=fig, idx=[1,3])
+fig = plot_output_hist(xvec, query.ntime; fig=fig, nbins=100, idx=[1,3])
