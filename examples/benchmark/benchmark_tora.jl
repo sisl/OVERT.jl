cfd01a52cb0a809282462a851dc3d1d2ddfbba05
Amir <amir.maleki.zamenjani@gmail.com>
2020-06-22 09:38:02 -0700

    small change in benchmark examples

--- examples/benchmark/benchmark_tora.jl
+++ examples/benchmark/benchmark_tora.jl
@@ -7,16 +7,18 @@ include("../../models/tora/tora.jl")
 
 query = OvertQuery(
 	Tora,                                     # problem
-	"nnet_files/controller_bigger_tora.nnet", # network file
-	ReLU(),                                   # last layer activation layer Id()=linear, or ReLU()=relu
+	"nnet_files/ARCH_COMP/controller_tora.nnet", # network file
+	ReLU(),                                        # last layer activation layer Id()=linear, or ReLU()=relu
 	"MIP",                                    # query solver, "MIP" or "ReluPlex"
-	10,                                       # ntime
-	1.0,                                      # dt
+	200,                                       # ntime
+	0.1,                                      # dt
 	-1,                                       # N_overt
 	)
 
 input_set = Hyperrectangle(low=[0.6, -0.7, -0.4, 0.5], high=[0.7, -0.6, -0.3, 0.6])
-all_sets =  many_timestep_concretization(query, input_set)[1]
+# all_sets =  many_timestep_concretization(query, input_set)[1]
+
+output_sets, xvec, x0 = monte_carlo_simulate(query, input_set; n_sim=100)
 
 # all_sets, all_sets_symbolic = symbolic_reachability(query, input_set)
 # output_sets, xvec, x0 = monte_carlo_simulate(query, input_set)
@@ -24,4 +26,4 @@ all_sets =  many_timestep_concretization(query, input_set)[1]
 # fig = plot_output_sets(all_sets)
 # fig = plot_output_sets([all_sets_symbolic]; linecolor=:red, fig=fig)
 # # fig = plot_output_hist(xvec, query.ntime; fig=fig, nbins=100)
-# fig = plot_mc_trajectories(x0, xvec; fig=fig, markersize=2)
+plot_mc_trajectories(x0, xvec; markersize=2)
