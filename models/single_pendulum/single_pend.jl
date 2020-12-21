9361857377cfbb8461db9c8c2a6e2583de529b4e
Chelsea Sidrane <csidrane@stanford.edu>
2020-12-21 12:35:20 -0500

    changing variable names, pulling symbolic expression out

--- models/single_pendulum/single_pend.jl
+++ models/single_pendulum/single_pend.jl
@@ -1,19 +1,21 @@
+pend_mass, pend_len, grav_const, friction = 0.5, 0.5, 1., 0.
+
 function single_pend_dynamics(x::Array{T, 1} where {T <: Real},
 	                          u::Array{T, 1} where {T <: Real})
-	m, l, g, c = 0.5, 0.5, 1., 0.
     dx1 = x[2]
-    dx2 = g/l * sin(x[1]) + 1 / (m*l^2) * (u[1] - c * x[2])
+    dx2 = grav_const/pend_len * sin(x[1]) + 1 / (pend_mass*pend_len^2) * (u[1] - friction * x[2])
     return [dx1, dx2]
 end
+
+single_pend_θ_doubledot = :($(grav_const/pend_len) * sin(x1) + $(1/(pend_mass*pend_len^2)) * u1 - $(friction/(pend_mass*pend_len^2)) * x2)
 
 function single_pend_dynamics_overt(range_dict::Dict{Symbol, Array{T, 1}} where {T <: Real},
 	                                N_OVERT::Int,
 									t_idx::Union{Int, Nothing}=nothing)
-	m, l, g, c = 0.5, 0.5, 1., 0.
 	if isnothing(t_idx)
-		v1 = :($(g/l) * sin(x1) + $(1/(m*l^2)) * u - $(c/(m*l^2)) * x2)
+		v1 = single_pend_θ_doubledot
 	else
-    	v1 = "$(g/l) * sin(x1_$t_idx) + $(1/(m*l^2)) * u_$t_idx - $(c/(m*l^2)) * x2_$t_idx"
+    	v1 = "$(grav_const/pend_len) * sin(x1_$t_idx) + $(1/(pend_mass*pend_len^2)) * u1_$t_idx - $(friction/(pend_mass*pend_len^2)) * x2_$t_idx"
     	v1 = Meta.parse(v1)
 	end
     v1_oA = overapprox_nd(v1, range_dict; N=N_OVERT)
@@ -29,7 +31,7 @@ function single_pend_update_rule(input_vars::Array{Symbol, 1},
 end
 
 single_pend_input_vars = [:x1, :x2]
-single_pend_control_vars = [:u]
+single_pend_control_vars = [:u1]
 
 SinglePendulum = OvertProblem(
 	single_pend_dynamics,
