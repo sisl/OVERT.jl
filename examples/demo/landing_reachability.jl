12bf47e971e6667cbd94055ca90a9e93d2ad07aa
Chelsea Sidrane <csidrane@stanford.edu>
2021-01-25 22:59:25 -0800

    minor bug that prevented saving

--- examples/demo/landing_reachability.jl
+++ examples/demo/landing_reachability.jl
@@ -16,7 +16,7 @@ query = OvertQuery(
     controller, # network file
     Id(),    # last layer activation layer Id()=linear, or ReLU()=relu
     "MIP",     # query solver, "MIP" or "ReluPlex"
-    45,        # ntime
+    5,        # ntime
     1,       # dt
     -1,        # N_overt
     )
@@ -24,10 +24,10 @@ query = OvertQuery(
                                 #xc,vc,yp,vp
 input_set = Hyperrectangle(low=[700,-15,-5, 99],
                             high=[750, -14, 5, 100])
-concretize_at = [10, 10, 10, 10, 5]
+concretize_at = [5] #[10, 10, 10, 10, 5]
 t1 = Dates.time()
 all_sets, all_sets_symbolic = symbolic_reachability_with_concretization(query, input_set, concretize_at)
 t2 = Dates.time()
 elapsed = (t2-t1)
 print("elapsed time = $(elapsed) seconds")
-JLD2.@save "landing_reachability_1_data.jld2" query input_set all_sets all_sets_symbolic dt 
+JLD2.@save "landing_reachability_1_data.jld2" query input_set all_sets all_sets_symbolic elapsed 
