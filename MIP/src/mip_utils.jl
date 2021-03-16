530e972ff82dfa5f67fbb8ad94388b41c288d095
csidrane <csidrane@stanford.edu>
2021-03-16 15:52:43 -0700

    Use dual to bound objective value of MIP optimization problem instead of using primal solution. This adds another small layer of overapproximation to ensure soundness

--- MIP/src/mip_utils.jl
+++ MIP/src/mip_utils.jl
@@ -169,11 +169,13 @@ function solve_for_reachability(mip_model::OvertMIP, query::OvertQuery,
 		@objective(mip_model.model, Min, next_v_mip)
 		JuMP.optimize!(mip_model.model)
 		@assert termination_status(mip_model.model) == MathOptInterface.OPTIMAL
-		push!(lows, objective_value(mip_model.model))
+		push!(lows, objective_bound(mip_model.model))
+		@debug "Objective value for MIN is: $(objective_value(mip_model.model)) and objective bound is $(objective_bound(mip_model.model))"
 		@objective(mip_model.model, Max, next_v_mip)
 		JuMP.optimize!(mip_model.model)
 		@assert termination_status(mip_model.model) == MathOptInterface.OPTIMAL
-		push!(highs, objective_value(mip_model.model))
+		push!(highs, objective_bound(mip_model.model))
+		@debug "Objective value for MAX is: $(objective_value(mip_model.model)) and objective bound is $(objective_bound(mip_model.model))"
 	end
 
 	# measurement model code, leave commented for now. til ready for integration
