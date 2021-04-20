ff9fa49636ca997682c5a45e83b9961dbe36c89e
csidrane <csidrane@stanford.edu>
2021-04-19 19:09:17 -0700

    reachability signle pend'

---
+++ examples/jmlr/single_pend_reach.jl
@@ -0,0 +1,39 @@
+include("../../models/problems.jl")
+include("../../OverApprox/src/overapprox_nd_relational.jl")
+include("../../OverApprox/src/overt_parser.jl")
+include("../../MIP/src/overt_to_mip.jl")
+include("../../MIP/src/mip_utils.jl")
+include("../../models/single_pendulum/single_pend.jl")
+
+controller = "nnet_files/jair/single_pendulum_small_controller.nnet"
+println("Controller: ", controller)
+query = OvertQuery(
+	SinglePendulum,    # problem
+	controller,        # network file
+	Id(),              # last layer activation layer Id()=linear, or ReLU()=relu
+	"MIP",             # query solver, "MIP" or "ReluPlex"
+	30,                # ntime
+	0.1,               # dt
+	-1,                # N_overt
+	)
+
+input_set = Hyperrectangle(low=[1., 0.], high=[1.2, 0.2])
+concretization_intervals = [10, 10, 10]
+t1 = Dates.time()
+concrete_state_sets, symbolic_state_sets, concrete_meas_sets, symbolic_meas_sets = symbolic_reachability_with_concretization(query, input_set, concretization_intervals)
+t2 = Dates.time()
+dt = (t2-t1)
+print("elapsed time= $(dt) seconds")
+
+# we want to check the intersection with the avoid set: x_1 <= -5
+avoid_set = [HalfSpace([1., 0.], -5.)] # 1*x_1 + 0*x_2 <= -5    => x_1 <= -5 
+
+init_set0, reachable_state_sets = clean_up_sets(concrete_state_sets, symbolic_state_sets, concretization_intervals)
+
+t1 = time()
+safe, violations = check_avoid_set_intersection(reachable_state_sets, input_set, avoid_set)
+dt_check = time() - t1
+
+using JLD2
+JLD2.@save "examples/jair/data/new2/single_pendulum_reachability_small_controller_data.jld2" query input_set concretization_intervals concrete_state_sets concrete_meas_sets symbolic_state_sets symbolic_meas_sets dt controller avoid_set reachable_state_sets safe violations dt_check
+ 
