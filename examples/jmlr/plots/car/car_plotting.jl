71f5d6a781b99142d669c53090aa633f38da5ded
Chelsea Sidrane <csidrane@stanford.edu>
2021-05-11 13:29:13 -0700

    car second type of plot

--- examples/jmlr/plots/car/car_plotting.jl
+++ examples/jmlr/plots/car/car_plotting.jl
@@ -14,15 +14,15 @@ include("../../../../OverApprox/src/overapprox_nd_relational.jl")
 include("../../../../OverApprox/src/overt_parser.jl")
 include("../../../../MIP/src/overt_to_mip.jl")
 include("../../../../MIP/src/mip_utils.jl")
-include("../../../../models/car/simple_car.jl")
+include("../../../../models/car_10step/simple_car.jl")
 
 controller = ARGS[1] # "smallest"
 
-one_step_data = load("examples/jmlr/data/car/car_reachability_$(controller)_controller_data_1step.jld2")
+one_step_data = load("examples/jmlr/data/car_10step/car_reachability_$(controller)_controller_data_1step.jld2")
 one_step_state_sets = one_step_data["symbolic_state_sets"] 
 one_step_meas_sets = one_step_data["symbolic_meas_sets"]
 
-data = load("examples/jmlr/data/car/car_reachability_$(controller)_controller_data.jld2")
+data = load("examples/jmlr/data/car_10step/car_reachability_$(controller)_controller_data.jld2")
 
 concrete_state_sets = vcat(data["concrete_state_sets"]...)
 symbolic_state_sets = vcat(data["symbolic_state_sets"]...)
@@ -41,11 +41,11 @@ define_color("symbolic_color", 0x9BFF85)
 define_color("mc_color", 0x38caff)
 define_color("goal_color", 0xa866ff)
 conc_style_solid = "solid, concrete_color, mark=none, fill=concrete_color"
-conc_style_transparent = conc_style_solid*", fill opacity=0.5"
+conc_style_transparent = conc_style_solid*", fill opacity=0.5, opacity=0.5"
 sym_style_solid = "solid, symbolic_color, mark=none, fill=symbolic_color"
-sym_style_transparent =  sym_style_solid*", fill opacity=0.5"
+sym_style_transparent =  sym_style_solid*", fill opacity=0.5, opacity=0.5"
 mc_style_solid = "solid, mc_color, mark=none, fill=mc_color"
-mc_style_transparent = "solid, red, mark=none, fill=red, fill opacity=0.5"#mc_style_solid*", fill opacity=0.5"
+mc_style_transparent = "solid, red, mark=none, fill=red, fill opacity=0.5, opacity=0.5"#mc_style_solid*", fill opacity=0.5"
 
 goal_style = "solid, goal_color, thick, mark=none, fill=goal_color"
 
@@ -57,15 +57,20 @@ goal_style = "solid, goal_color, thick, mark=none, fill=goal_color"
 dims=[1,2]
 fig = PGFPlots.Axis(style="width=10cm, height=10cm", ylabel="\$y\$", xlabel="\$x\$", title="Car Position Reachable Sets")
 
-# hack for legend
-push!(fig, PGFPlots.Plots.Linear([9., 9.], [-4., -4.], style=mc_style_solid, legendentry="Monte Carlo Simulations convex hull"))
-
 # push!(fig, PGFPlots.Plots.Linear([9., 9.], [-4., -4.], style=mc_style_transparent, legendentry="Monte Carlo Simulations hyperrectangular hull"))
 
 # plot init set in both concrete and hybrid colors
 inputx, inputy = get_rectangle(data["input_set"], dims)
 push!(fig, PGFPlots.Plots.Linear(inputx, inputy, style=conc_style_transparent, legendentry="Concrete Sets"))
 push!(fig, PGFPlots.Plots.Linear(inputx, inputy, style=sym_style_transparent, legendentry="OVERT Hybrid Symbolic Sets"))
+
+# hack for legend
+push!(fig, PGFPlots.Plots.Linear([9., 9.], [-4., -4.], style=mc_style_solid, legendentry="Monte Carlo Simulations convex hull"))
+
+# plot goal set 
+goal_set = Hyperrectangle(low=[-0.6, -0.2], high=[0.6, 0.2])
+goal_x, goal_y = get_rectangle(goal_set, dims)
+push!(fig, PGFPlots.Plots.Linear(goal_x, goal_y, style=goal_style, legendentry="Goal Set"))
 
 ### plot init set of mc simulations
 points = x0[:, dims]
@@ -74,11 +79,6 @@ border_idx = chull(points).vertices
 x = points[border_idx, 1]
 y = points[border_idx, 2]
 push!(fig, PGFPlots.Plots.Linear([x..., x[1]], [y..., y[1]], style=mc_style_solid))
-
-# plot goal set 
-# goal_set = Hyperrectangle(low=[-0.6, -0.2], high=[0.6, 0.2])
-# goal_x, goal_y = get_rectangle(goal_set, dims)
-# push!(fig, PGFPlots.Plots.Linear(goal_x, goal_y, style=goal_style, legendentry="Goal Set"))
 
 for t in 1:data["query"].ntime
     if t == data["query"].ntime
@@ -109,3 +109,34 @@ fig.legendStyle =  "at={(1.05,1.0)}, anchor=north west"
 
 PGFPlots.save("examples/jmlr/plots/car/car_$(controller)_x12.tex", fig)
 PGFPlots.save("examples/jmlr/plots/car/car_$(controller)_x12.pdf", fig)
+
+#########################################
+##### plot 2: SAT trajectory  ######
+#########################################
+# counter examples are "easy" here because no trajectory reaches the goal :'D
+# load sat data 
+q1_data = load("examples/jmlr/data/car_10step/car_satisfiability_$(controller)_controller_data_q1.jld2")
+q2_data = load("examples/jmlr/data/car_10step/car_satisfiability_$(controller)_controller_data_q2.jld2")
+final_data = load("examples/jmlr/data/car_10step/car_satisfiability_$(controller)_controller_data_final_result.jld2")
+# final data contains the SATuses "sat" or "unsat" for each timestep. 
+# if one of the queries has multiple timesteps that were sat, this produces multiple possible counterexample "hints"
+# we take the first timestep values of any "sat" step, and simulate them forward to get a real counterexample. we could do this for every timestep that returns SAT
+x0 = [q2_data["statii"][1][Symbol("$(v)_1")] for v in q2_data["query"].problem.input_vars] 
+xvec_mc = monte_carlo_one_simulate(q2_data["query"], x0)
+
+fig = PGFPlots.Axis(style="width=10cm, height=10cm", ylabel="\$y\$", xlabel="\$x\$", title="Car Counter Example")
+
+dims=[1,2]
+# plot goal set 
+goal_set = Hyperrectangle(low=[-0.6, -0.2], high=[0.6, 0.2])
+goal_x, goal_y = get_rectangle(goal_set, dims)
+push!(fig, PGFPlots.Plots.Linear(goal_x, goal_y, style=goal_style, legendentry="Goal Set"))
+
+counter_example_style = "concrete_color, solid, mark=*, mark options={concrete_color, fill=concrete_color}"
+# plot the counter example 
+push!(fig, PGFPlots.Plots.Linear(xvec_mc[:, dims[1]], xvec_mc[:, dims[2]], style=counter_example_style, legendentry="Counter Example"))
+
+fig.legendStyle =  "at={(1.05,1.0)}, anchor=north west"
+
+PGFPlots.save("examples/jmlr/plots/car/car_$(controller)_counter_example.tex", fig)
+PGFPlots.save("examples/jmlr/plots/car/car_$(controller)_counter_example.pdf", fig)
