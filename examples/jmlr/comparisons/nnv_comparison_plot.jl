7c18e7f340e2f69c7128176af0f9cccb3793d2c1
Chelsea Sidrane <csidrane@stanford.edu>
2021-05-28 17:30:33 -0700

    comparison plots

--- examples/jmlr/comparisons/nnv_comparison_plot.jl
+++ examples/jmlr/comparisons/nnv_comparison_plot.jl
@@ -2,7 +2,13 @@ using PGFPlots
 using QHull
 using DelimitedFiles
 using JLD2
+using FileIO
 using LazySets
+include("models/problems.jl")
+include("OverApprox/src/OA_relational_util.jl")
+include("MIP/src/overt_to_mip.jl")
+include("MIP/src/mip_utils.jl")
+include("models/single_pendulum/single_pend.jl")
 
 # plot comparison between nnv and overt
 # decided to compare actually, the original experiments. not smaller, modified ones.
@@ -28,34 +34,36 @@ for points in nnv_reach_set_vertices
 end
 
 # load OVERT data
+overt_data = load("examples/jmlr/data/single_pend_-0.2167/single_pendulum_reachability_small_controller_data.jld2")
+init_state, overt_reach_sets = clean_up_sets(overt_data["concrete_state_sets"], overt_data["symbolic_state_sets"], overt_data["concretization_intervals"])
 
 # plot styles
 define_color("symbolic_color", 0x9BFF85)
 define_color("nnv_color", 0xbc86d1)
 nnv_style = "solid, nnv_color, mark=none, fill=nnv_color"
 nnv_style_trans = nnv_style*", fill opacity=0.5"
+overt_style = "solid, symbolic_color, mark=none, fill=symbolic_color"
+overt_style_trans = overt_style*", fill opacity=0.5"
 
 # make PGFPlot
 fig = PGFPlots.Axis(style="width=8cm, height=8cm", xlabel="\$x_1\$", ylabel="\$x_2\$", title="Single Pendulum Reachable Sets, Comparison")
 
 # avoid set
-push!(fig, PGFPlots.Plots.Linear([-0.2617, -0.2617], [-0.9, 0.5], style="red, dashed", mark="none", legendentry="Unsafe Set"))
+push!(fig, PGFPlots.Plots.Linear([-0.2617, -0.2617], [-10., 10.], style="red, dashed", mark="none", legendentry="Unsafe Set"))
 
 # plot init set
 push!(fig, PGFPlots.Plots.Linear(nnv_reach_sets[1][:,1], nnv_reach_sets[1][:,2], style=overt_style, legendentry="OVERT"))
 
 push!(fig, PGFPlots.Plots.Linear(nnv_reach_sets[1][:,1], nnv_reach_sets[1][:,2], style=nnv_style, legendentry="nnv"))
 
-for t in 1:20
-    # if t == 20
-    #     push!(fig, PGFPlots.Plots.Linear(nnv_reach_sets[t+1][:,1], nnv_reach_sets[t+1][:,2], style=nnv_style))
-    #     #push!(fig, PGFPlots.Plots.Linear( get_rectangle(one_step_state_sets[t], dims)..., style=conc_style))
-    #     #push!(fig, PGFPlots.Plots.Linear( get_rectangle(reachable_state_sets[t], dims)..., style=sym_style))
-    # else
-        push!(fig, PGFPlots.Plots.Linear(nnv_reach_sets[t+1][:,1], nnv_reach_sets[t+1][:,2], style=nnv_style_trans))
-        #push!(fig, PGFPlots.Plots.Linear( get_rectangle(one_step_state_sets[t], dims)..., style=conc_style_trans))
-        #push!(fig, PGFPlots.Plots.Linear( get_rectangle(reachable_state_sets[t], dims)..., style=sym_style_trans))
-    # end
+for t in 1:10
+    push!(fig, PGFPlots.Plots.Linear(nnv_reach_sets[t+1][:,1], nnv_reach_sets[t+1][:,2], style=nnv_style_trans))
+    #push!(fig, PGFPlots.Plots.Linear(get_rectangle(overt_reach_sets[t], [1,2])..., style=overt_style_trans))
+end
+
+for t in 1:10
+    #push!(fig, PGFPlots.Plots.Linear(nnv_reach_sets[t+1][:,1], nnv_reach_sets[t+1][:,2], style=nnv_style_trans))
+    push!(fig, PGFPlots.Plots.Linear(get_rectangle(overt_reach_sets[t], [1,2])..., style=overt_style_trans))
 end
 
 fig.legendStyle = "at={(1.05,1.0)}, anchor=north west"
