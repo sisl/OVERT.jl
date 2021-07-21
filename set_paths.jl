3246773fe1eae8cc0aca2288e7b5176be9ab53bc
Chelsea <chelseasidrane@gmail.com>
2021-07-21 13:30:08 -0700

    cleaning

--- set_paths.jl
+++
@@ -1,7 +0,0 @@
-function setpaths()
-  rootdir = @__DIR__
-  #mipdir = joinpath(rootdir,"MIP")
-  #overtdir = joinpath(rootdir,"OverApprox")
-  push!(LOAD_PATH, rootdir)
-end
-setpaths();
--- test.jl
+++
@@ -1,6 +0,0 @@
-# testing script.
-
-# run this script before committing.
-# fix any tests that you have broken. 
-
-include("sound/sound_tests.jl")
