2d2f43d464dee26c9b82782dd32f3af24e7d62ad
Mykel Kochenderfer <mykel@stanford.edu>
2021-08-11 08:21:29 -0700

    test example notebook

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,10 @@
 # run all tests
 
+using NBInclude
+
 include("d2f_zeros_test.jl")
 include("overapprox_nd_unittest.jl")
+
+@testset "test methods notebook" begin
+    @nbinclude(joinpath(dirname(@__DIR__), "plots", "Methods Section.ipynb"))
+end
