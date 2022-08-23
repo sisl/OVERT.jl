58db444505c724618533c5484dd142d217453511
chelseas <csidrane@stanford.edu>
2022-08-23 14:18:45 -0700

    comment about relative error tol

--- src/overapprox_nd.jl
+++ src/overapprox_nd.jl
@@ -31,7 +31,7 @@ end
     -> OverApproximation
 Overapproximate an n-dimensional function using a relational abstraction.
 
-N    is the number of linear segments used per region of constant convexity. To allow OVERT to automatically choose this number, set N to -1. It will add segments until the maximum deviation between the function and the bound is within 2%.
+N    is the number of linear segments used per region of constant convexity. To allow OVERT to automatically choose this number, set N to -1. It will add segments until the maximum deviation between the function and the bound is within the relative error tolerance.
 
 ϵ    is the RELATIVE "air gap" added to the bounds so that the bounds don't touch the function at the point of closest approach. If the function bound lies in the range [a,b] a gap of size ϵ*(b-a) will be added to the y values of all points of the upper bound and subtracted from all points of the lower bound. The default value is 1% or ϵ = 0.01
 """
